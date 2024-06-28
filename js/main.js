1746ea8b24e1bff061cf8ac44f8699e1177f8d33
Phil <96127866+PhilMOB@users.noreply.github.com>
2024-06-28 14:23:18 +0200

    Première mise à jour de la journée

--- js/main.js
+++ js/main.js
@@ -24,7 +24,13 @@ if(navClose){
 
 /*=============== ADD BLUR HEADER ===============*/
 
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
